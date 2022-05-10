2aabd47455c22ef03d7f5ebd2302ca829ad700ef
Héctor Buruca <torrezhectorb@gmail.com>
2022-05-09 21:15:59 -0600

    terminado

---
+++ ejercicio2.js
@@ -0,0 +1,14 @@
+for (let i = 0; i <= 10; i++) {
+    if (i === 2) {
+        console.log("Numero primo")
+
+    } else if (i === 3) {
+        console.log("Numero primo")
+    } else if (i === 5) {
+        console.log("Numero primo")
+    } else if (i === 7) {
+        console.log("Numero primo")
+    } else {
+        console.log(i);
+    }
+}
